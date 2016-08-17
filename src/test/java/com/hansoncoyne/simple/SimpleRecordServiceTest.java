844f5c4a22e6c73fea3f6a2acb0f95956541e30d
Norm Hanson <nhanson@nhanson-lt.local>
2016-08-17 01:03:41 -0500

    unused imports

--- src/test/java/com/hansoncoyne/simple/SimpleRecordServiceTest.java
+++ src/test/java/com/hansoncoyne/simple/SimpleRecordServiceTest.java
@@ -4,8 +4,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
